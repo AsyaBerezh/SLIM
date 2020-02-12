8a97fe1d42e889de97c46ece30890a9cd05e54e8
Anastasia <Asya.berezhnaya@gmail.com>
2020-02-12 14:58:50 +0200

    changes to driver

--- src/test/java/org/slim/ui/BaseTest.java
+++ src/test/java/org/slim/ui/BaseTest.java
@@ -23,13 +23,15 @@ public class BaseTest {
 
         System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        /*
         options.addArguments("start-maximized"); // open Browser in maximized mode
         options.addArguments("disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
+        //options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--no-sandbox"); // Bypass OS security model
         options.addArguments("--headless");
+        */
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://slim-accp.mendixcloud.com/admin.html");
         //  Configuration.browser =  "chrome";
