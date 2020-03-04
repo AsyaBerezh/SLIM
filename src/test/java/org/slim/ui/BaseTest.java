dfd39fe078deadb3ffdcb2228406856bd83a4d85
Anastasia <Asya.berezhnaya@gmail.com>
2020-03-04 13:18:33 +0200

    Added a lot of new things for driver and configuration

--- src/test/java/org/slim/ui/BaseTest.java
+++ src/test/java/org/slim/ui/BaseTest.java
@@ -7,32 +7,35 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import com.codeborne.selenide.WebDriverRunner;
 public class BaseTest {
     LoginPage loginPage = new LoginPage();
-
+    EventFiringWebDriver driver;
     @BeforeSuite
     public void preSuite(){
 
         //Configuration.browser = "chrome";
-      Configuration.browser = "firefox"; //for local
+        // Configuration.browser = "firefox"; //for local
 /*    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
     System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();*/
 
-/*        System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("--headless");
-        WebDriver driver = new ChromeDriver(options);*/
+        //  System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver.exe");
 
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
+        System.setProperty("webdriver.chrome.driver", getPathToDriver());
+        WebDriver webDriver = new ChromeDriver(options);
+        webDriver.manage().window().maximize();
+        driver = new EventFiringWebDriver(webDriver);
       //  Configuration.browser =  "chrome";
        // Configuration.timeout =  20000;
         Configuration.reportsFolder = "target/test-result/reports";
@@ -40,15 +43,26 @@ public class BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.get(Configuration.browser =  "chrome");*/
     }
-
+    private String getPathToDriver() {
+        String osName = System.getProperty("os.name").toLowerCase();
+        if(osName.contains("win")) {
+            return this.getClass().getResource("/chromedriver_windows32/chromedriver.exe").getPath();
+        }
+        if(osName.contains("mac")) {
+            return this.getClass().getResource("/chromedriver_mac64/chromedriver").getPath();
+        }
+        if(osName.contains("nux")) {
+            return "src/test/resources/chromedriver_linux64/chromedriver";
+        }
+        throw new RuntimeException("Your system type was not detected! Please download and set chromedriver by yourself");
+    }
     @BeforeClass
     private void preClass (){
         loginPage
                 .loginToDashboard("anastasia@fitsoft.nl", "QWEqwe123!@#");
     }
-/*
-    @AfterSuite
-    public void tearDown() {
-        WebDriverRunner.getWebDriver().close();
-    }*/
+    @AfterClass
+    protected void tearDown() {
+        driver.quit();
+    }
 }
